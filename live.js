7ec42fbca4135f70933c86b1f50f3e673bb55a10
David Pitman <david.pitman@cloudouble.com>
2023-03-14 00:26:09 +1000

    refactoring for initial undocumented release

--- live.js
+++ live.js
@@ -34,57 +34,46 @@ const Live = Object.defineProperties({}, {
         result || await this._runListener(key, input, force, idempotent, verbose)
     }}, 
     _runListener: {configurable: false, enumerable: false, writable: false, value: async function(key, input={}, force=false, idempotent=false, verbose=false) {
-        const listener = this.listeners[key] || {processor: key}, processorKey = listener.processor || key 
-        let processor = typeof this.processors[processorKey] == 'object' ? this.processors[processorKey].listener : this.processors[processorKey]
-        processor = typeof processor == 'function' ? processor : input => input
-        if ((listener instanceof Object) && (force || (!force && !listener.expired && !listener.maxed)))  {
-            const now = Date.now()
-            if (force || !listener.period || (listener.period && (((listener.previous || 0) + listener.period) <= now))) {
-                if (!force && !listener.expired && (listener.expires && (listener.expires <= now))) {
-                    listener.expired = true
-                    globalThis.dispatchEvent(new CustomEvent(`b37ListenerExpired`, {detail: {listener: key, input: input}}))
-                    globalThis.dispatchEvent(new CustomEvent(`b37ListenerExpired-${key}`, {detail: {listener: key, input: input}}))
-                } else {
-                    input = input instanceof Object ? input : (listener.input instanceof Object ? listener.input : {})
-                    if (!idempotent) {
-                        listener.previous = now
-                        listener.count = (listener.count || 0) + 1
-                        const previous = listener.previous, count = listener.count
-                        listener.next = now + listener.period
-                    }
-                    const result = await processor(input)
-                    globalThis.dispatchEvent(new CustomEvent(`b37-listener-run`, {detail: {listener: key, result: result}}))
-                    globalThis.dispatchEvent(new CustomEvent(`b37-listener-run-${key}`, {detail: {listener: key, result: result}}))
-                    if (listener.max && !listener.maxed && (listener.count == listener.max)) {
-                        listener.maxed = true
-                        globalThis.dispatchEvent(new CustomEvent(`b37-listener-maxed`, {detail: {listener: key, input: input}}))
-                        globalThis.dispatchEvent(new CustomEvent(`b37-listener-maxed-${key}`, {detail: {listener: key, input: input}}))
-                    }
-                    if (listener.expires && listener.period && ((now + listener.period) >= listener.expires)) {
-                        listener.expired = true
-                        globalThis.dispatchEvent(new CustomEvent(`b37ListenerExpired`, {detail: {listener: key, input: input}}))
-                        globalThis.dispatchEvent(new CustomEvent(`b37ListenerExpired-${key}`, {detail: {listener: key, input: input}}))
-                    }
+        const listener = this.listeners[key] || {processor: key}, processorKey = listener.processor || key , 
+            processor = this.processors[processorKey]?.listener || this.processors[processorKey] || input => input, now = Date.now()
+        if (!((listener instanceof Object) && (force || (!force && !listener.expired && !listener.maxed)))) return true  
+        if (force || !listener.period || (listener.period && (((listener.previous || 0) + listener.period) <= now))) {
+            if (!force && !listener.expired && (listener.expires && (listener.expires <= now))) {
+                listener.expired = true
+                globalThis.dispatchEvent(new CustomEvent(`b37ListenerExpired`, {detail: {listener: key, input: input}}))
+                globalThis.dispatchEvent(new CustomEvent(`b37ListenerExpired-${key}`, {detail: {listener: key, input: input}}))
+            } else {
+                input = (input instanceof Object && input) || (listener.input instanceof Object && listener.input) || {}
+                if (!idempotent) {
+                    listener.previous = now
+                    listener.count = (listener.count || 0) + 1
+                    const previous = listener.previous, count = listener.count
+                    listener.next = now + listener.period
                 }
-            } else if (!force && listener.period && (listener.next && (listener.next > now))) {
-                if (verbose || listener.verbose) {
-                    globalThis.dispatchEvent(new CustomEvent(`b37-listener-passed`, {detail: {listener: key, input: input}}))
-                    globalThis.dispatchEvent(new CustomEvent(`b37-listener-passed-${key}`, {detail: {listener: key, input: input}}))
-                }
+                const result = await processor(input)
+                globalThis.dispatchEvent(new CustomEvent(`b37ListenerRun`, {detail: {listener: key, result: result}}))
+                globalThis.dispatchEvent(new CustomEvent(`b37ListenerRun-${key}`, {detail: {listener: key, result: result}}))
+                listener.max && !listener.maxed && (listener.count == listener.max) && (listener.maxed = true) 
+                    && globalThis.dispatchEvent(new CustomEvent(`b37ListenerMaxed`, {detail: {listener: key, input: input}}))
+                    && globalThis.dispatchEvent(new CustomEvent(`b37ListenerMaxed-${key}`, {detail: {listener: key, input: input}}))
+
+                listener.expires && listener.period && ((now + listener.period) >= listener.expires) && (listener.expired = true)
+                    && globalThis.dispatchEvent(new CustomEvent(`b37ListenerExpired`, {detail: {listener: key, input: input}}))
+                    && globalThis.dispatchEvent(new CustomEvent(`b37ListenerExpired-${key}`, {detail: {listener: key, input: input}}))
+            }
+        } else if (!force && listener.period && (listener.next && (listener.next > now))) {
+            if (verbose || listener.verbose) {
+                globalThis.dispatchEvent(new CustomEvent(`b37-listener-passed`, {detail: {listener: key, input: input}}))
+                globalThis.dispatchEvent(new CustomEvent(`b37-listener-passed-${key}`, {detail: {listener: key, input: input}}))
             }
         }
         return true
     }},
     _run: {configurable: false, enumerable: false, writable: false, value: function($this) {
-        Object.entries($this.listeners).forEach(entry => {
-            if (entry[1].period) {
-                $this._runListener(entry[0])
-            }
-        })
+        for (const k in $this.listeners) if ($this.listeners[k].period) $this._runListener(k)
         const processElement = function(element, type) {
             const attrName = type === 'subscription' ? 'b37-from' : 'b37-to'
-            const cleanVectors = [], listAttribute = (element.getAttribute(attrName) || ''), 
-                flags = type == 'subscription' ? $this._subscriptions: $this._triggers
+            const cleanVectors = [], listAttribute = (element.getAttribute(attrName) || ''), flags = $this[`_${type}s`]
             let firstPass = false, id = element.getAttribute('b37-id'), listAttributeValueChanged
             if (!element.hasAttribute('b37-id')) {
                 firstPass = true
@@ -92,87 +81,69 @@ const Live = Object.defineProperties({}, {
                 id = crypto.randomUUID()
                 element.setAttribute('b37-id', id)
             } else if (!firstPass && id && flags[id] instanceof Object 
-                && Object.keys(flags[id]).sort().join(' ') != listAttribute) {
+                && Object.keys(flags[id]).sort().join(' ') !== listAttribute) {
                 firstPass = true
             }
             let vectorList = listAttribute.split(' ')
-            if (firstPass) {
-                vectorList = vectorList.sort().filter(v => !!v)
-            }
+            if (firstPass) vectorList = vectorList.sort().filter(v => !!v)
             if (firstPass && flags[id] instanceof Object) {
-                Object.keys(flags[id]).forEach(vector => {
+                for (const vector in flags[id]) {
                     const vectorSplit = vector.split(':')
                     if (!vectorList.includes(vector)) {
-                        const eventType = type == 'subscription' ? `b37ListenerRun-${vectorSplit[0]}` : vectorSplit[0]
-                        const listeningElement =  type == 'subscription' ? window : element
+                        const eventType = `${type===subscription?'b37ListenerRun-':''}${vectorSplit[0]}`, 
+                            listeningElement =  type == 'subscription' ? window : element
                         listeningElement.removeEventListener(eventType, flags[id][vector])
-                    }
-                })
+                    }                    
+                }
             }
-            vectorList.forEach(vector => {
+            for (const vector of vectorList) {
                 if (firstPass) {
                     const originalVector = vector
                     let colonIndex = vector.indexOf(':')
-                    if (colonIndex != -1) { vector = vector.replace(/:+/g, ':') }
+                    if (colonIndex != -1) vector = vector.replace(/:+/g, ':')
                     colonIndex = vector.indexOf(':')
-                    if (colonIndex === 0) { vector = vector.slice(1) }
+                    if (colonIndex === 0) vector = vector.slice(1)
                     colonIndex = vector.indexOf(':')
                     if (colonIndex == -1) { vector = `${vector}:${vector}` } else if (colonIndex == vector.length-1) { vector = `${vector}${vector}` }
-                    listAttributeValueChanged = originalVector != vector
+                    listAttributeValueChanged = originalVector !== vector
                     cleanVectors.push(vector)
                 }
-                flags[id] = flags[id] instanceof Object ? flags[id] : {}
+                if (!(flags[id] instanceof Object)) flags[id] = {}
                 if (!flags[id][vector]) {
                     const vectorSplit = vector.split(':')
                     flags[id][vector] = async function(event) {
                         const processor = (typeof $this.processors[vectorSplit[1]] == 'object' && typeof $this.processors[vectorSplit[1]][type] == 'function') ? $this.processors[vectorSplit[1]][type] : $this.processors[vectorSplit[1]]
-                        if (typeof processor == 'function') {
-                            const handlerInput =  type == 'subscription' ? {...event.detail, ...{subscriber: element}} 
-                                : {
-                                    ...event.detail, 
-                                    ...{
-                                        attributes: Object.assign({}, ...Array.from(element.attributes).map(a => ({[a.name]: a.value}))), 
-                                        properties: {value: element.value, innerHTML: element.innerHTML, innerText: element.innerText}, 
-                                        map: {
-                                            ...Object.assign({}, ...Array.from(element.attributes).map(a => ({[`@${a.name}`]: a.value}))), 
-                                            ...{'#value': element.value, '#innerHTML': element.innerHTML, '#innerText': element.innerText}
-                                        }, 
-                                        triggersource: element, 
-                                        event: event,
-                                        vector: vector
-                                    }
-                                }
-                            const handledPayload = await processor(handlerInput)
-                            if (type == 'subscription' && handledPayload instanceof Object) {
-                                Object.keys(handledPayload).forEach(k => {
-                                    handledPayload[k] = handledPayload[k] === undefined ? '' : (handledPayload[k] === null ? '' : handledPayload[k])
-                                    if (k && k[0] == '#') {
-                                        element[k.slice(1)] = handledPayload[k]
-                                    } else if (k && k[0] == '@') {
-                                        element.setAttribute(k.slice(1), handledPayload[k])
-                                    } else {
-                                        element.setAttribute(k, handledPayload[k])
-                                    }
-                                })
-                            }
+                        if (typeof processor !== 'function') return
+                        const handlerInput =  type == 'subscription' ? {...event.detail, ...{subscriber: element}} 
+                            : {...event.detail, 
+                                ...{
+                                    attributes: Object.assign({}, ...Array.from(element.attributes).map(a => ({[a.name]: a.value}))), 
+                                    properties: {value: element.value, innerHTML: element.innerHTML, innerText: element.innerText}, 
+                                    map: {
+                                        ...Object.assign({}, ...Array.from(element.attributes).map(a => ({[`@${a.name}`]: a.value}))), 
+                                        ...{'#value': element.value, '#innerHTML': element.innerHTML, '#innerText': element.innerText}
+                                    }, 
+                                    triggersource: element, 
+                                    event: event,
+                                    vector: vector
+                                }}
+                        const handledPayload = await processor(handlerInput)
+                        if (!(type == 'subscription' && handledPayload instanceof Object)) return
+                        for (const k in handledPayload) {
+                            handledPayload[k] ?? (handledPayload[k] ||= '')
+                            k && k[0] == '#' && (element[k.slice(1)] = handledPayload[k])
+                            k && k[0] == '@' && element.setAttribute(k.slice(1), handledPayload[k])
+                            if (!k || !'#@'.includes(k[0])) element.setAttribute(k, handledPayload[k])
                         }
                     }
-                    const eventType = type == 'subscription' ? `b37ListenerRun-${vectorSplit[0]}` : vectorSplit[0], listeningElement =  type == 'subscription' ? window : element
+                    const eventType = `${type===subscription?'b37ListenerRun-':''}${vectorSplit[0]}`, listeningElement =  type == 'subscription' ? window : element
                     listeningElement.addEventListener(eventType, flags[id][vector])
                 }
-            })
-            if (firstPass && listAttributeValueChanged) {
-                element.setAttribute(attrName, cleanVectors.sort().join(' '))
-            } else if (firstPass && (listAttribute != vectorList.join(' '))) {
-                element.setAttribute(attrName, vectorList.sort().join(' '))
             }
+            firstPass && element.setAttribute(attrName, (listAttributeValueChanged?cleanVectors:vectorList).sort().join(' '))
         }
-        document.querySelectorAll(`[b37-from]`).forEach(subscribedElement => {
-            processElement(subscribedElement, 'subscription')
-        })
-        document.querySelectorAll(`[b37-to]`).forEach(triggeringElement => {
-            processElement(triggeringElement, 'trigger')
-        })
+        for (const subscribedElement of document.querySelectorAll(`[b37-from]`)) processElement(subscribedElement, 'subscription')
+        for (const triggeringElement of document.querySelectorAll(`[b37-to]`)) processElement(triggeringElement, 'trigger')
         globalThis.requestIdleCallback(function() { $this._run($this) }, {options: $this.maxDelay || 1000})
     }}
 })
